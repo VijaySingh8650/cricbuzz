3ee6aeb50fe89addac493e46ae79b8f3b1d1b182
Vijay_singh <rathorevijay995@gmail.com>
2022-06-15 07:44:33 +0530

    navbar completed

---
+++ components/navbar2.js
@@ -0,0 +1,18 @@
+let navbar2 = ()=>{
+   return ` <div>
+
+        
+   <a href="" id="match">Matches</a>
+   <a href="" title="India v South Africa, 3rd T20I - Live" class="nav2_match">IND vs RSA - Live</a>
+   <a href="" title="Sri Lanka v Australia, 1st ODI - Live" class="nav2_match">SL vs AUS - Live</a>
+   <a href="" title="New Zealand v England, 2nd Test - Live" class="nav2_match">NZ vs ENG - Live</a>
+   <a href="" title="Afghanistan v Zimbabwe, 3rd T20I - AFG Won" class="nav2_match">AFG vs ZIM - AFG Won</a>
+   <a href="" title="Madhya Pradesh v Bengal, 1st Semi-Final - Stumps" class="nav2_match">MP vs BEN - Stumps</a>
+</div>
+<div>
+<a href="" id="all">ALL</a>
+<i class="fa-solid fa-caret-down"></i>
+</div>`;
+}
+
+export default navbar2;
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
-import navbar from "/components/navbar.js";
+import navbar from "../components/navbar.js";
 let navbarAppend = document.getElementById("navbar1");
 navbarAppend.innerHTML=navbar();
+
+// console.log(navbar());
+import navbar2 from "../components/navbar2.js";
+let navbarAppend2 = document.getElementById("navbar2");
+navbarAppend2.innerHTML=navbar2();
